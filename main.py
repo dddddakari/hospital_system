a7811ea1870334004a2b944fc75b79cca0a3c1cb
Derek Sow <dddddakari@gmail.com>
2024-08-12 20:12:38 -0600

    Adding the doctors :)
    It's just the dictionary for the doctors, took way longer than expected lol.

---
+++ main.py
@@ -0,0 +1,45 @@
+doctors = {
+    21: {
+        "name": "Dr.Gody",
+        "specialization": "ENT",
+        "working_time": "5am-11am",
+        "qualification": "MBBS,MD",
+        "room_number": 17
+    },
+    32: {
+        "name": "Dr.Vikram",
+        "specialization": "Physician",
+        "working_time": "10am-3pm",
+        "qualification": "MBBS,MD",
+        "room_number": 45
+    },
+    17: {
+        "name": "Dr.Amy",
+        "specialization": "Surgeon",
+        "working_time": "8am-2pm",
+        "qualification": "BDM",
+        "room_number": 8
+    },
+    33: {
+        "name": "Dr.David",
+        "specialization": "Artho",
+        "working_time": "10am-4pm",
+        "qualification": "MBBS,MS",
+        "room_number": 40
+    },
+    123: {
+        "name": "Dr.Ross",
+        "specialization": "Headaches",
+        "working_time": "8am-10am",
+        "qualification": "Mst",
+        "room_number": 102
+    },
+    66: {
+        "name": "Dr.Mike",
+        "specialization": "Heart",
+        "working_time": "9am-5pm",
+        "qualification": "MS",
+        "room_number": 2
+    }
+}
+
