e42a63ad39012046097be3956b9551b21a443109
Derek Sow <dddddakari@gmail.com>
2024-08-12 22:06:09 -0600

    Adds Display Menu Options
    I haven't done the patients so I will keep that for later.

--- main.py
+++ main.py
@@ -148,7 +148,16 @@ class Management:
     def display_menu(self):
         print("Welcome to Alberta Hospital (AH) Management system \n Select from the following options, or select 3 to stop: \n 1 - Doctors \n 2 - Patients \n 3 - Exit Program \n")
         choice = input(">>> ")
+        if choice =='1':
+            self.doctors_menu()
+        elif choice == '3':
+            print("Thanks for using the program. Bye!")
+        else:
+            print("Invalid Choice, Please Choose from 1, 2 or 3!")
 
     def doctors_menu(self):
         print("\n Doctors Menu: \n 1- Display Doctors List \n 2 - Search for doctor by ID \n 3 - Search for doctor by name \n 4 - Add doctor \n 5 - Edit doctor info \n 6 - Back to the Main Menu  ")
         choice = input(">>> ")
+        if choice == "1":
+            self.doctor_manager.display_doctors_list()
+        elif choice ==
